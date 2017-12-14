f773e44a6736a642465e8bd67bed554572b56ce4
nadianm <nadia.nouimehidi@gmail.com>
2017-12-13 20:13:16 -0600

    sql connection & orm stuff

---
+++ controllers/userController.js
@@ -0,0 +1,6 @@
+// Pull in required dependencies
+var express = require('express');
+var router = express.Router();
+
+// Import the model (user.js) to use its database functions.
+var User = require('../models/user.js');
