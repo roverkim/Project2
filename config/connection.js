32917a7745366ea7445ce4cbf88bc740c4aff33c
Roverkim <ryankim@hotmail.com>
2017-12-17 15:41:06 -0600

    Removed Files

--- config/connection.js
+++ config/connection.js
@@ -7,17 +7,15 @@ var connection;
 if (process.env.JAWSDB_URL) {
 // DB is JawsDB on Heroku
 connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-      } else {
+} else {
 // DB is local on localhost
-          connection = mysql.createConnection({
-          port: 3306,
-          host: 'gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-          user: 'k44glo6u371o50eg',
-          password: 'r3gzdfcbsnwesjcd',
-          database: 'bpu19iau6iiinpq5'
-        })
-
+connection = mysql.createConnection({
+  port: 3306,
+   host: 'gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+   user: 'k44glo6u371o50eg',
+   password: 'r3gzdfcbsnwesjcd',
+   database: 'bpu19iau6iiinpq5'
+})
 };
 
 // Make the connection to MySQL
