fae8a0f01bc297ef7148975284d5a8285b3ffce9
Roverkim <ryankim@hotmail.com>
2017-12-17 15:14:20 -0600

    UI Changes

---
+++ public/assets/js/frontPage.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+
+  /////////////////////////////////////////////// /* SVG */ //////////////////////////////////////////////////////////
+  // Activate Drop Down Menu
+  $(".dropdown-button").dropdown();
+
+  // Activate Youtube modal
+  $('.youtubeModal').modal();
+
+  // Changes the main page background. 8 images every 5 seconds
+  function run(interval, frames) {
+      var int = 1;
+
+      function func() {
+          document.body.id = "bg"+int;
+          int++;
+          if(int === frames) { int = 1; }
+      }
+
+      var swap = window.setInterval(func, interval);
+  }
+
+  run(20000, 9); //milliseconds, frames
+
+  /////////////////////////////////////////////// /* Google Authentication*/ //////////////////////////////////////////////////////////
+  function onSignIn(googleUser) {
+    // Useful data for client-side scripts:
+    var profile = googleUser.getBasicProfile();
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://localhost:8080/api/signin');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+      console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('idtoken=' + id_token);
+  };
+
+});
