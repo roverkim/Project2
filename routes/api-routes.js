3a601d3e9343b193c672566463b745429b604d27
Paige Lo <sk8asd123@gmail.com>
2017-12-31 17:39:15 -0600

    removed favicon and eslint-airbnb package

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,18 @@
-var connection = require("../config/connection.js")
+var db = require("../models");
+
+module.exports = function(app) {
+
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+};
